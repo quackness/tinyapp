17fcd081e31e6eebc5b686737c3dbba06178a475
Karolina Redden <dubajkaro@gmail.com>
2022-03-02 15:41:49 +0000

    2nd push for header partial to account for all changes

--- express_server.js
+++ express_server.js
@@ -24,7 +24,10 @@ app.get('/urls', (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]  };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]
+  };
   res.render('urls_show', templateVars);
 });
 
